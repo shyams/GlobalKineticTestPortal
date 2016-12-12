b52f6316740d526ce943f09384f6a1deb42b74aa
shyamsunder <shyam.0385@gmail.coom>
2016-12-12 02:17:36 +0200

    Test Portal

---
+++ src/java/WebService/TestWebservice.java
@@ -0,0 +1,163 @@
+package WebService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author shyam
+ */
+public class TestWebservice extends HttpServlet {
+//    protected String endpoint = "http://localhost:8080/GlobalKineticTestService/webresources/users";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        StringBuilder sb = new StringBuilder();
+        try {
+            String endpoint = "http://" + request.getServerName() + ":" + request.getServerPort() + "/GlobalKineticTestService/webresources/users";
+            URL url = new URL(endpoint);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                sb.append(output);
+            }
+            response.getWriter().print(sb.toString());
+            br.close();
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String output = makeRequest(request, "POST");
+            System.out.print(output + "helloo");
+            response.getWriter().print(output);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String res = makeRequest(request, "PUT");
+            response.getWriter().print(res);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected HttpResponse makePutRequest(HttpServletRequest request) {
+        String endpoint = "http://" + request.getServerName() + ":" + request.getServerPort() + "/GlobalKineticTestService/webresources/users";
+        HttpResponse httpRes = null;
+        try {
+            StringBuilder data = new StringBuilder();
+            String str = null;
+            while ((str = request.getReader().readLine()) != null) {
+                data.append(str);
+            }
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPut putRequest = new HttpPut(endpoint);
+            System.out.println("String " + data.toString());
+            StringEntity input = new StringEntity(data.toString());
+            input.setContentType("application/json");
+            putRequest.setEntity(input);
+
+            httpRes = httpClient.execute(putRequest);
+            System.err.println(httpRes.getStatusLine() + "done");
+            httpClient.getConnectionManager().shutdown();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (httpRes != null) {
+            return httpRes;
+        } else {
+            return null;
+        }
+
+    }
+
+    protected String makeRequest(HttpServletRequest request, String requestType) {
+        StringBuilder outputdata = null;
+
+        try {
+            String endpoint = "http://" + request.getServerName() + ":" + request.getServerPort() + "/GlobalKineticTestService/webresources/users";
+            System.out.println(endpoint);
+            URL url = new URL(endpoint);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod(requestType);
+            conn.setRequestProperty("Content-Type", "application/json");
+            StringBuilder data = new StringBuilder();
+            String str = null;
+            while ((str = request.getReader().readLine()) != null) {
+                data.append(str);
+            }
+
+            String input = data.toString();
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+            outputdata = new StringBuilder();
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+                outputdata.append(output);
+            }
+
+            conn.disconnect();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+        if (outputdata != null) {
+            return outputdata.toString();
+        } else {
+            return "";
+        }
+
+    }
+}
---
+++ web/js/processIndex.js
@@ -0,0 +1,138 @@
+
+function hideDivs() {
+    $("#loginPage").addClass("hidden");
+    $("#usersLoggedIn").addClass("hidden");
+    $("#registerPage").addClass("hidden");
+    $("#successAlert").addClass("hidden");
+}
+$(document).ready(function () {
+    var idleTime;
+    hideDivs();
+    $("#registerPage").removeClass("hidden");
+    $("#loginPage").removeClass("hidden");
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+    function timerIncrement() {
+        idleTime = idleTime + 1;
+        console.log("idle for", idleTime);
+        if (idleTime >= 3) { // 3 minutes
+            console.log("We logging you out from the app");
+            var strJSON = '{"token":"' + $("a#logoutBtn").attr("sessionID") + '"}';
+            $.post("TestWebservice", strJSON, function (data, status) {
+                if (status === "success") {
+                    var jsonData = JSON.parse(data);
+                    hideDivs();
+                    window.location.reload();
+                }
+            });
+        }
+    }
+});
+
+
+$("#register").click(function () {
+
+    var phone = $("#phoneToRegister").val();
+    var pass = $("#passwordToRegister").val();
+    var user = $("#usernameToRegister").val();
+    if (!isNaN(phone) && phone != "" && pass != "" && user != "") {
+        $.ajax({
+            url: 'TestWebservice',
+            type: 'PUT',
+            data: '{"username":"' + user + '","password":"' + pass + '","phone":"' + phone + '"}',
+            success: function (data, status) {
+                console.log(data);
+                if (status === "success") {
+                    hideDivs();
+                    window.location.reload();
+                }
+            }
+        });
+    }
+    return false;
+});
+
+$("#login").click(function () {
+    console.log("clicked");
+    var pass = $("#passwordToLogin").val();
+    var user = $("#usernameToLogin").val();
+    if (pass != "" && user != "") {
+        var strJSON = '{"username":"' + user + '","password":"' + pass + '"}';
+        $.post("TestWebservice", strJSON, function (data, status) {
+            console.log(data);
+            if (status === "success") {
+                var jsonData = JSON.parse(data);
+                hideDivs();
+                if (jsonData.id == undefined) {
+                    window.location.reload();
+                } else if (jsonData.id != undefined && jsonData.id != null) {
+                    $("#loggerDetails").html("Signed in as: " + jsonData.id);
+                    $("a#logoutBtn").attr("sessionID", jsonData.token)
+                    getLoggersDetails();
+                }
+            }
+        });
+    }
+    return false;
+});
+
+function getLoggersDetails() {
+    $.get("TestWebservice", function (data, status) {
+
+        if (status === "success") {
+
+            hideDivs();
+            var jsonData = JSON.parse(data);
+            var users = jsonData.users;
+            $('#userTable tbody').html("");
+            $('#loggedInFiveMin tbody').html("");
+            for (var i = 0; i < users.length; i++) {
+                var count = i + 1;
+                var user = users[i];
+
+                var phone = user.phone
+                var name = user.id;
+                var time = user.time;
+                $("#usersLoggedIn").removeClass("hidden");
+
+                $('#userTable tbody').append('<tr><th scope="row">' + count + '</th><td>' + name + '</td><td>' + phone + '</td></tr>');
+                var date = new Date();
+                var millisec = date.getTime();
+                var minDiff = (time - millisec) / 60 / 1000; //in minutes
+                console.log(minDiff,time,millisec);
+                if (minDiff >= -5) {
+                    $('#loggedInFiveMin tbody').append('<tr><th scope="row">' + count + '</th><td>' + name + '</td><td>' + phone + '</td></tr>')
+                }
+                
+
+            }
+        }
+    })
+}
+
+$("a#logoutBtn").click(function () {
+    var strJSON = '{"token":"' + $("a#logoutBtn").attr("sessionID") + '"}';
+    $.post("TestWebservice", strJSON, function (data, status) {
+        console.log(data);
+        if (status === "success") {
+            var jsonData = JSON.parse(data);
+            hideDivs();
+            window.location.reload();
+        }
+    });
+});
+
+function checkUserLoggedIn() {
+    console.log("checking");
+    if (($("a#logoutBtn").attr("sessionID")) != undefined) {
+        console.log("logged in");
+        getLoggersDetails();
+    }
+}
+setInterval(checkUserLoggedIn, (10 * 1000));
